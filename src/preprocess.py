8dffc7e748524355d4de66439a924a5684535356
Germán Poo-Caamaño <gpoo@gnome.org>
2017-12-22 22:45:19 -0300

    Save the images and log the progress

--- src/preprocess.py
+++ src/preprocess.py
@@ -2,15 +2,16 @@
 
 import sys
 import argparse
+import logging
 from os import listdir
 from os.path import isfile, isdir, join, basename
+from datetime import datetime
 
 from wand.image import Image
 from wand.display import display
 from wand.color import Color
 
 name = 'foo - Convert pictures to full HD image size'
-
 
 def get_metadata(f):
     ORIENTATION = 'exif:Orientation'
@@ -29,49 +30,49 @@ def get_metadata(f):
             orientation = 'Portrait' if o in (6, 8) else 'Landscape'
         return w, h, o, d, m, orientation
 
-def process_fujifilm(f, w, h, o, d, m, o2):
+def process_fujifilm(img, w, h, o):
+    if o == 8 and h < w:
+        img.rotate(-90)
+    elif o == 6 and h < w:
+        img.rotate(90)
+    elif o == 3:
+        img.rotate(180)
+
+    return img
+
+
+def process_lge(img, w, h, o):
+    return img
+
+
+def process(f, w, h, o, d, m, o2, output):
     black = Color('black')
     width = 1920
     height = 1080
 
-    with Image(filename=f) as img:
-        if o == 8 and h < w:
-            img.rotate(-90, background=black)
-        elif o == 6 and h < w:
-            img.rotate(90, background=black)
-        elif o == 3:
-            img.rotate(180, background=black)
+    with Image(filename=f) as f:
+        if m == 'FUJIFILM':
+            img = process_fujifilm(f, w, h, o)
+        elif m == 'LGE':
+            img = process_lge(f, w, h, o)
+        else:
+            logging.warning('Unknown camera {}'.format(m))
+
         img.transform(resize='{}x{}'.format(width, height))
+
         with Image(width=width, height=height, background=black) as out:
             out.format = img.format.lower()
             out.composite(img, left=int((width - img.width)/ 2),
                           top=int((height - img.height) / 2))
-            display(out)
+            logging.info(output)
+            if isfile(output):
+                logging.warning('{} already exists!, skiping'.format(output))
+            else:
+                out.save(filename=output)
 
-
-def process_lge(f, w, h, o, d, m, o2):
-    black = Color('black')
-    width = 1920
-    height = 1080
-    with Image(filename=f) as img:
-        img.transform(resize='{}x{}'.format(width, height))
-        with Image(width=width, height=height, background=black) as out:
-            out.format = img.format.lower()
-            out.composite(img, left=int((width - img.width)/ 2),
-                          top=int((height - img.height) / 2))
-            display(out)
-
-
-def process(f, w, h, o, d, m, o2):
-    if m == 'FUJIFILM':
-        process_fujifilm(f, w, h, o, d, m, o2)
-    elif m == 'LGE':
-        process_lge(f, w, h, o, d, m, o2)
-    else:
-        print('Unknown camera {}'.format(m))
-
-
-def main(args):
+def main(args, dstdir):
+    dt_from_filter = datetime.strptime('2016:12:24 0:0:0',
+                                       '%Y:%m:%d %H:%M:%S')
     files = []
     for i in args:
         if isdir(i):
@@ -81,17 +82,22 @@ def main(args):
             files.append(i)
 
     for i in sorted(files):
-        print(basename(i), end=': ')
         w, h, o, d, m, o2 = get_metadata(i)
-        print('{}x{} {} {} {} {}'.format(w, h, o, d, m, o2))
-        process(i, w, h, o, d, m, o2)
-
+        logging.info('{} {}x{} {} {} {} {}'.format(basename(i),
+                                                   w, h, o, d, m, o2))
+        dt = datetime.strptime(d, '%Y:%m:%d %H:%M:%S')
+        if dt >= dt_from_filter:
+            output = join(dstdir, dt.strftime('%Y%m%d-%H%M%S.jpg'))
+            process(i, w, h, o, dt, m, o2, output)
 
 def start():
+
     args = argparse.ArgumentParser(description=name)
 
     args.add_argument('source', nargs='+',
                       help='Source directory')
+    args.add_argument('--output', default='/tmp',
+                      help='Directory to save the resulting images')
 
     opts = args.parse_args()
 
@@ -99,8 +105,10 @@ def start():
         sources = [s.strip() for s in sys.stdin.readlines()]
     else:
         sources = opts.source
+    dstdir = opts.output
 
-    main(sources)
+    main(sources, dstdir)
 
 if __name__ == '__main__':
+    logging.basicConfig(format='%(levelname)s:%(message)s')
     start()
