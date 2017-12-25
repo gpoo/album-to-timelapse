eb8c9b669e40565c1b0449d7e3f58ab6188a5d5a
Germán Poo-Caamaño <gpoo@gnome.org>
2017-12-25 18:42:39 -0300

    Adjust the level of logging

--- src/preprocess.py
+++ src/preprocess.py
@@ -125,5 +125,6 @@ def start():
     main(sources, dstdir)
 
 if __name__ == '__main__':
-    logging.basicConfig(format='%(levelname)s:%(message)s')
+    logging.basicConfig(format='%(levelname)s:%(message)s',
+                        level=logging.INFO)
     start()
